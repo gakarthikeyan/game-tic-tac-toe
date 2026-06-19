387da928d7ced185305749c2b2a799c3df0d68e6
Karthikeyan <karthiganesan.gk@gmail.com>
2026-06-19 10:20:31 +0530

    [Tic-Tac-Toe] Added case for winning row is false (X, O, X)

--- app/src/test/java/com/kata/android/game_tic_tac_toe/domain/model/GameBoardTest.kt
+++ app/src/test/java/com/kata/android/game_tic_tac_toe/domain/model/GameBoardTest.kt
@@ -4,7 +4,15 @@ import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
-
+/**
+ * Game Tic-Tac-Toe with TDD approach.
+ *
+ * Test all the cells of the board. Check if a player has won,
+ * if the board is full, or if the game is still ongoing.
+ * Already occupied cases, etc.
+ *
+ * @author Karthikeyan G
+ */
 class GameBoardTest {
 
     @Test
@@ -70,6 +78,18 @@ class GameBoardTest {
             )
         )
         assertTrue(gameBoard.hasWinningRow(GamePlayer.X))
+    }
+
+    @Test
+    fun `player without winning row returns false`() {
+        val gameBoard = GameBoard(
+            cells = arrayOf(
+                GamePlayer.X, GamePlayer.O, GamePlayer.X,
+                null, null, null,
+                null, null, null
+            )
+        )
+        assertFalse(gameBoard.hasWinningRow(GamePlayer.X))
     }
 
 
